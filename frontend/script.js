85a8e5f11e49ba9db1cd66db8d487eca288469c8
svelehorski <svelehorski@gmail.com>
2026-03-11 21:55:16 -0300

    Agregar función mostrar y botones para operaciones matemáticas en HTML

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,23 @@
-function mostrar(){
-    alert("putin el que lenin");
+function sumar(){
+    let texto = parseInt(document.getElementById("input").value);
+    let texto2 = parseInt(document.getElementById("output").value);
+    alert(texto + texto2);
+    //document.getElementById("titulo").textContent = texto2;
 }
+
+function restar(){
+    let input1 = parseInt(document.getElementById("input").value);
+    let input2 = parseInt(document.getElementById("output").value);
+    alert(input1 - input2);
+}
+
+function multiplicar(){
+    let input1 = parseInt(document.getElementById("input").value);
+    let input2 = parseInt(document.getElementById("output").value);
+    alert(input1 * input2);
+}
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
 
